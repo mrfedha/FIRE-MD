3aa5c8b6054b641eb54b80c674658c26fba6ddbb
mrfedha <eddyfedha5@gmail.com>
2024-10-31 20:05:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
-global.allowed = ['2349013432338', '2349013432338', '2349013432338']
+global.mods = ['254796774447', '254796774447']
+global.prems = ['254796774447', '254796774447', '254796774447']
+global.allowed = ['254796774447', '254796774447', '254796774447']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
